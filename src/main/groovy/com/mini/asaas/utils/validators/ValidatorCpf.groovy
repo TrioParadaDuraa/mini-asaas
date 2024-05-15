82bdc686134fbbf9a5f6a83f7733bb563252095c
NayaneBatista <nayanebatistaa@gmail.com>
2024-05-15 14:15:56 -0300

    Início da implementação dos validators em Utils com o atributo CPF

---
+++ src/main/groovy/com/mini/asaas/utils/validators/ValidatorCpf.groovy
@@ -0,0 +1,17 @@
+package com.mini.asaas.utils.validators
+
+class CpfValidator {
+
+    public static boolean isValidCpf(String cpf) {
+        if (cpf.length() != 11) {
+            return false
+        }
+
+        if (cpf.replaceAll("(.)\\1*", "").length() == 1) {
+            return false
+        }
+      
+        return true
+    }
+}
+// campos para criar validação dps: cnpj, email, cep 
