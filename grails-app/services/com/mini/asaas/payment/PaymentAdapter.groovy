4b60211e7979cf22f2ab2d37be09a4dfd7e8b5f2
Joao Victor Cardozo <joao.cardozo@asaas.com.br>
2024-05-17 14:06:11 -0300

    Criação do payment adapter

--- grails-app/services/com/mini/asaas/payment/PaymentAdapter.groovy
+++ grails-app/services/com/mini/asaas/payment/PaymentAdapter.groovy
@@ -1,4 +1,22 @@
 package com.mini.asaas.payment
 
+import com.mini.asaas.payer.Payer
+import com.mini.asaas.utils.base.PaymentStatus
+import com.mini.asaas.utils.base.PaymentType
+
 class PaymentAdapter {
+
+    Payer payer
+    PaymentType paymentType
+    BigDecimal value
+    PaymentStatus status
+    Date dueDate
+
+    public PaymentAdapter(Map params) {
+        this.payer = params.payer
+        this.paymentType = PaymentType.convert(params.paymentType.toUpperCase())
+        this.value = params.value
+        this.status = params.status
+        this.dueDate = params.dueDate
+    }
 }
