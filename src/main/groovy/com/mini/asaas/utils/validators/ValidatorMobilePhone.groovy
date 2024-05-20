8e89f23d32ff860a262561ae21a015bff5bc0da7
Nayane Batista <70235376+NayaneBatista@users.noreply.github.com>
2024-05-20 10:50:05 -0300

    Correção do acesso a string com charAt
    A indexação entre colchetes não é suportada para strings em Groovy.

--- src/main/groovy/com/mini/asaas/utils/validators/ValidatorMobilePhone.groovy
+++ src/main/groovy/com/mini/asaas/utils/validators/ValidatorMobilePhone.groovy
@@ -2,7 +2,7 @@ package com.mini.asaas.utils.validators
 
 class ValidatorMobilePhone {
     public static boolean isValidMobilePhone(String mobilePhone) {
-        if (mobilePhone.matches("[0-9]{11}") && mobilePhone[2] == '9') {
+        if (mobilePhone.matches("[0-9]{11}") && mobilePhone.charAt(2) == '9') {
             return true
         }
 
