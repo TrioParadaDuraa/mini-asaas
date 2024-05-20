50a7e72b61adc849231f5402666bd818c376c83e
Nayane Batista <70235376+NayaneBatista@users.noreply.github.com>
2024-05-20 14:22:47 -0300

    Melhoria do módulo "isValidCpfCnpj" e correção de espaços

--- src/main/groovy/com/mini/asaas/utils/validators/ValidatorCpfCnpj.groovy
+++ src/main/groovy/com/mini/asaas/utils/validators/ValidatorCpfCnpj.groovy
@@ -3,6 +3,10 @@ package com.mini.asaas.utils.validators
 class ValidatorCpfCnpj {
 
     static boolean isValidCpfCnpj(String cpfCnpj) {
+        if (!cpfCnpj.matches("\\d{11}") && !cpfCnpj.matches("\\d{14}")) {
+            return false
+        }
+    
         if (cpfCnpj.length() == 11) {
             return isValidCpf(cpfCnpj)
         } else if (cpfCnpj.length() == 14) {
@@ -13,10 +17,6 @@ class ValidatorCpfCnpj {
     }
 
     private static boolean isValidCpf(String cpf) {
-        if (cpf.length() != 11) {
-            return false
-        }
-
         if (cpf.replaceAll("(.)\\1*", "").length() == 1) {
             return false
         }
@@ -25,7 +25,9 @@ class ValidatorCpfCnpj {
         (0..8).each { i ->
             sum += Integer.parseInt(cpf[i]) * (10 - i)
         }
+        
         def digit1 = (sum * 10) % 11
+        
         if (digit1 == 10) digit1 = 0
 
         if (digit1 != Integer.parseInt(cpf[9])) {
@@ -36,7 +38,9 @@ class ValidatorCpfCnpj {
         (0..9).each { i ->
             sum += Integer.parseInt(cpf[i]) * (11 - i)
         }
+        
         def digit2 = (sum * 10) % 11
+        
         if (digit2 == 10) digit2 = 0
 
         if (digit2 != Integer.parseInt(cpf[10])) {
@@ -47,19 +51,17 @@ class ValidatorCpfCnpj {
     }
 
     private static boolean isValidCnpj(String cnpj) {
-        if (cnpj.length() != 14) {
-            return false
-        }
-
         if (cnpj.replaceAll("(.)\\1*", "").length() == 1) {
             return false
         }
 
         def sum = 0
+        
         def weights = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
         (0..11).each { i ->
             sum += Integer.parseInt(cnpj[i]) * weights[i]
         }
+        
         def digit1 = (sum % 11 < 2) ? 0 : (11 - sum % 11)
 
         if (digit1 != Integer.parseInt(cnpj[12])) {
@@ -71,6 +73,7 @@ class ValidatorCpfCnpj {
         (0..12).each { i ->
             sum += Integer.parseInt(cnpj[i]) * weights[i]
         }
+        
         def digit2 = (sum % 11 < 2) ? 0 : (11 - sum % 11)
 
         if (digit2 != Integer.parseInt(cnpj[13])) {
