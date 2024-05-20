942b27816fa8d50b0b965534b6120bea39656891
Nayane Batista <70235376+NayaneBatista@users.noreply.github.com>
2024-05-20 10:45:49 -0300

    Corrige e melhora o validator de CEP

--- src/main/groovy/com/mini/asaas/utils/validators/ValidatorPostalCode.groovy
+++ src/main/groovy/com/mini/asaas/utils/validators/ValidatorPostalCode.groovy
@@ -3,11 +3,7 @@ package com.mini.asaas.utils.validators
 class PostalCodeValidator {
 
     public static boolean isValidPostalCode(String postalCode) {
-        if (postalCode.length() != 8) {
-            return false
-        }
-
-        if (postalCode.replaceAll("(.)\\1*", "").length() == 1) {
+        if (!postalCode.matches("\\d{8}") || postalCode.matches("(.)\\1{7}")) {
             return false
         }
 
