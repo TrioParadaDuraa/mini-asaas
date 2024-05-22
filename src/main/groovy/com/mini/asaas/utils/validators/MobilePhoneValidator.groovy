4f66d25f3c9c75ac45cb599af7e8780d4c262b4a
Nayane Batista <70235376+NayaneBatista@users.noreply.github.com>
2024-05-22 11:22:50 -0300

    Renomeia o file e a class

--- src/main/groovy/com/mini/asaas/utils/validators/MobilePhoneValidator.groovy
+++ src/main/groovy/com/mini/asaas/utils/validators/MobilePhoneValidator.groovy
@@ -1,6 +1,6 @@
 package com.mini.asaas.utils.validators
 
-class ValidatorMobilePhone {
+class MobilePhoneValidator {
     public static boolean isValidMobilePhone(String mobilePhone) {
         if (mobilePhone.matches("[0-9]{11}") && mobilePhone.charAt(2) == '9') {
             return true
