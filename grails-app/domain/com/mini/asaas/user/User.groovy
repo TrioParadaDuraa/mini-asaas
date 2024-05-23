327d39da6cc7f2d7ec5d257b5629095816b4a56f
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2024-05-23 17:56:14 -0300

    Cria a configuração inicial do Spring Security

---
+++ grails-app/domain/com/mini/asaas/user/User.groovy
@@ -0,0 +1,46 @@
+package com.mini.asaas.user
+
+import com.mini.asaas.customer.Customer
+import com.mini.asaas.domain.base.BaseDomain
+
+import grails.compiler.GrailsCompileStatic
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@GrailsCompileStatic
+@EqualsAndHashCode(includes='username')
+@ToString(includes='username', includeNames=true, includePackage=false)
+class User extends BaseDomain implements Serializable {
+
+    private static final long serialVersionUID = 1
+
+    String username
+
+    String password
+
+    Customer customer
+
+    boolean enabled = true
+
+    boolean accountExpired
+
+    boolean accountLocked
+
+    boolean passwordExpired
+
+    Set<Role> getAuthorities() {
+        (UserRole.findAllByUser(this) as List<UserRole>)*.role as Set<Role>
+    }
+
+    static constraints = {
+        username nullable: false, blank: false, email: true
+        password nullable: false, blank: false, password: true
+    }
+
+    static mapping = {
+        table '`user`'
+        username unique: true
+	    password column: '`password`'
+    }
+}
