db030a8e7d42120664485865637d0fd4bb9edb1f
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2024-05-24 15:41:53 -0300

    Permite apresentar dados de um User

--- grails-app/controllers/com/mini/asaas/user/UserController.groovy
+++ grails-app/controllers/com/mini/asaas/user/UserController.groovy
@@ -11,15 +11,30 @@ class UserController {
     def save() {
         try {
             Long customerId = 1
-
+            
             UserAdapter adapter = new UserAdapter(params)
             
             User user = userService.save(adapter, customerId)
 
-            redirect(action: "index")
+            redirect(action: "show", id: user.id)
         } catch (Exception exception) {
             log.error("UserController.save >> Erro ao cadastrar usuário", exception)
             render "Não foi possível cadastrar o usuário"
+        }
+    }
+
+    def show() {
+        try {
+            User user = User.read(params.long("id"))
+
+            if (!user) {
+                throw new Exception("Usuário não encontrado")
+            }
+
+            return [user: user]
+        } catch (Exception exception) {
+            log.error("UserController.show >> Erro ao tentar apresentar dados de usuário", exception)
+            render "Usuário não encontrado"
         }
     }
 }
---
+++ grails-app/views/user/show.gsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualizar Usuário</title>
+</head>
+<body>
+    <form>
+        <div>
+            <label for="email">Email</label>
+            <br>
+            <input name="email" value="${user.username}" id="email" type="email">
+        </div>
+    </form>
+</body>
+</html>
