0fb3563648a48b3273c6072c271498ac33150e0b
NayaneBatista <nayanebatistaa@gmail.com>
2024-05-29 10:18:28 -0300

    Adiciona mensagem de exception e verificação dos pagamentos

--- grails-app/jobs/com/mini/asaas/DueDatePaymentJob.groovy
+++ grails-app/jobs/com/mini/asaas/DueDatePaymentJob.groovy
@@ -3,6 +3,7 @@ package com.mini.asaas
 import com.mini.asaas.payment.Payment
 import com.mini.asaas.utils.enums.PaymentStatus
 import grails.compiler.GrailsCompileStatic
+import org.springframework.transaction.annotation.Transactional
 
 @GrailsCompileStatic
 class DueDatePaymentJob {
@@ -10,12 +11,22 @@ class DueDatePaymentJob {
         cron name: 'dueDatePaymentJob', cronExpression: '0 0 0 * *?'
     }
 
+    @Transactional
     def execute() {
         List<Payment> overduePayments = Payment.findAllByDueDateLessThanOrEqualTo(new Date())
 
+        if (overduePayments.isEmpty()) {
+            log.info("Não foram encontradas cobranças vencidas")
+            return
+        }
+
         overduePayments.each { payment ->
-            payment.status = PaymentStatus.OVERDUE
-            payment.save(flush: true)
+            try {
+                payment.status = PaymentStatus.OVERDUE
+                payment.save(flush: true)
+            } catch (Exception exception) {
+                log.error("Erro ao atualizar o status do pagamento ${payment.id}: ${exception.message}")
+            }
         }
     }
 }
