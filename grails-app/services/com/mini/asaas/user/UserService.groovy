c085922297d574dc1430a3f7f215b97479327009
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2024-05-31 12:59:34 -0300

    Cria método validateSave em UserService

--- grails-app/services/com/mini/asaas/user/UserService.groovy
+++ grails-app/services/com/mini/asaas/user/UserService.groovy
@@ -2,15 +2,23 @@ package com.mini.asaas.user
 
 import com.mini.asaas.customer.Customer
 
+import com.mini.asaas.utils.validators.EmailValidator
+import com.mini.asaas.utils.validators.PasswordValidator
+
 import grails.compiler.GrailsCompileStatic
 import grails.gorm.transactions.Transactional
+import grails.validation.ValidationException
 
 @GrailsCompileStatic
 @Transactional
 class UserService {
 
     public User saveCustomerUser(UserAdapter adapter, Long customerId) {
-        User user = new User()
+        User user = validateSave(adapter)
+
+        if (user.hasErrors()) {
+            throw new ValidationException("Erro ao criar usuário", user.errors)
+        }
 
         user.customer = Customer.read(customerId)
         user.username = adapter.username
@@ -21,6 +29,22 @@ class UserService {
 
         Role role = Role.findByAuthority("ROLE_CUSTOMER")
         UserRole.create(user, role)
+
+        return user
+    }
+
+    private User validateSave(UserAdapter adapter) {
+        User user = new User()
+
+        if (!EmailValidator.isValidEmail(adapter.username)) {
+            user.errors.reject("username", null, "Email inválido")
+        }
+
+        if (!PasswordValidator.matches(adapter.password, adapter.passwordConfirm)) {
+            user.errors.reject("password", null, "Confirmação de senha incorreta")
+        } else if (!PasswordValidator.isStrong(adapter.password)) {
+            user.errors.reject("password", null, "A senha não atende aos requisitos mínimos")
+        }
 
         return user
     }
--- src/main/groovy/com/mini/asaas/utils/validators/PasswordValidator.groovy
+++ src/main/groovy/com/mini/asaas/utils/validators/PasswordValidator.groovy
@@ -11,15 +11,19 @@ class PasswordValidator {
 
     public static Closure validate() {
         return { String password, User user ->
-            return password == user.passwordConfirm && isStrong(password)
+            return matches(password, user.passwordConfirm) && isStrong(password)
         }
     }
 
-    public static Boolean isStrong(String password) {
+    public static boolean matches(String password, String passwordConfirm) {
+        return password == passwordConfirm
+    }
+
+    public static boolean isStrong(String password) {
         return (password.size() >= MIN_PASSWORD_SIZE) && hasCharsAndDigits(password)
     }
 
-    private static Boolean hasCharsAndDigits(String password) {
+    private static boolean hasCharsAndDigits(String password) {
         return password.find(/[a-zA-Z]/) && password.find(/[0-9]/)
     } 
 }
