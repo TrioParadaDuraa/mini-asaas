26d37f1a37c1966869dc62a02983c339dfb03afe
Nayane Batista <70235376+NayaneBatista@users.noreply.github.com>
2024-06-03 16:06:27 -0300

    Correção na namedQuery

--- grails-app/domain/com/mini/asaas/payment/Payment.groovy
+++ grails-app/domain/com/mini/asaas/payment/Payment.groovy
@@ -43,7 +43,7 @@ class Payment extends BaseDomain {
             }
         }
 
-        outstandingOverdueIds {
+        overdueIds {
             projections {
                 property('id')
             }
