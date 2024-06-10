274d4dfd97804900d48c6eb3f5de84a98b274c06
Pedro A. da Silva <pedrosilvafsa12@gmail.com>
2024-06-10 14:34:36 -0300

    Cria NotificationService

---
+++ grails-app/services/com/mini/asaas/notification/NotificationService.groovy
@@ -0,0 +1,63 @@
+package com.mini.asaas.notification
+
+import com.mini.asaas.payment.Payment
+
+import com.mini.asaas.utils.enums.PaymentStatus
+
+import grails.events.annotation.Subscriber
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class NotificationService {
+
+    @Subscriber
+    void save(Payment payment) {
+        Notification notification = new Notification()
+
+        notification.customer = payment.customer
+        notification.payment = payment
+        notification.title = "Cobrança criada"
+        notification.description = "Uma nova cobrança foi criada."
+
+        notification.save(failOnError: true)
+    }
+
+    @Subscriber
+    void delete(Payment payment) {
+        payment.attach()
+
+        Notification notification = new Notification()
+
+        notification.customer = payment.customer
+        notification.payment = payment
+        notification.title = "Cobrança excluída"
+        notification.description = "Uma cobrança foi excluída."
+
+        notification.save(failOnError: true)
+    }
+
+    @Subscriber
+    void updateStatus(Payment payment) {
+        payment.attach()
+
+        if (payment.status == PaymentStatus.OVERDUE) {
+            Notification notification = new Notification()
+
+            notification.customer = payment.customer
+            notification.payment = payment
+            notification.title = "Cobrança vencida"
+            notification.description = "Uma cobrança passou da data de vencimento."
+
+            notification.save(failOnError: true)
+        } else if (payment.status in [PaymentStatus.RECEIVED, PaymentStatus.RECEIVED_IN_CASH]) {
+            Notification notification = new Notification()
+
+            notification.customer = payment.customer
+            notification.payment = payment
+            notification.title = "Pagamento recebido"
+            notification.description = "O pagamento de uma cobrança foi recebido."
+
+            notification.save(failOnError: true)
+        }        
+    }
+}
