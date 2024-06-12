8df0338ccf2a055d80b5b961b35a0af6a4aa37af
Joao Victor Cardozo <danichs1one@gmail.com>
2024-06-12 11:19:51 -0300

    Listagem de valores registrados no sistema, contagem de pagadores, contagem de cobranças e soma de valores a receber, recebidos

--- grails-app/controllers/com/mini/asaas/dashboard/DashboardController.groovy
+++ grails-app/controllers/com/mini/asaas/dashboard/DashboardController.groovy
@@ -1,5 +1,10 @@
 package com.mini.asaas.dashboard
 
+import com.mini.asaas.payer.Payer
+import com.mini.asaas.payment.Payment
+
+import com.mini.asaas.utils.enums.PaymentStatus
+
 import grails.compiler.GrailsCompileStatic
 import grails.plugin.springsecurity.annotation.Secured
 
@@ -8,6 +13,18 @@ class DashboardController {
 
     @Secured("isAuthenticated()")
     def index() {
+        List <Payer> payerList = Payer.list()
 
+        List <Payment> paymentList = Payment.list()
+
+        List <Payment> overduePayments = paymentList.findAll {it.status == PaymentStatus.OVERDUE}
+
+        List <Payment> receivedPayments = paymentList.findAll {it.status == PaymentStatus.RECEIVED || it.status == PaymentStatus.RECEIVED_IN_CASH}
+
+        def valuesToReceive = paymentList.sum {it.value ?: 0}
+
+        def valuesReceived = receivedPayments.sum {it.value ?: 0}
+
+        return [payerList: payerList, paymentList: paymentList, overduePayments: overduePayments, receivedPayments: receivedPayments, valuesToReceive: valuesToReceive, valuesReceived: valuesReceived]
     }
 }
